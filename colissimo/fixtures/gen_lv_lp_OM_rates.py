7354496920a71d831850c3629b2ed3aa23c91bae
pierreben <pierrebenjamin@free.fr>
2017-01-13 17:39:31 +0100

    MAINT: Updates for january 2017

--- colissimo/fixtures/gen_lv_lp_OM_rates.py
+++ colissimo/fixtures/gen_lv_lp_OM_rates.py
@@ -1,11 +1,11 @@
 from decimal import Decimal as D
 
 paliers = [
-		   (D(0.02), D(0.80)),
-		   (D(0.1), D(1.60)),
-		   (D(0.25), D(3.2)),
-		   (D(0.5), D(4.8)),
-		   (D(3.00), D(6.4)),
+		   (D(0.02), D(0.73)),
+		   (D(0.1), D(1.46)),
+		   (D(0.25), D(2.92)),
+		   (D(0.5), D(4.38)),
+		   (D(3.00), D(5.84)),
 		   ]
 
 pas, supplement = D(0.01), D(0.11)
@@ -22,4 +22,4 @@ for p, palier_price in paliers:
 		current_val += pas
 
 for weight, rate in valeurs:
-	print '%s, %s, 0,0,0,3,9' % (weight, rate)
+	print '%s, %s, 0,0,0,3,13' % (weight, rate)
